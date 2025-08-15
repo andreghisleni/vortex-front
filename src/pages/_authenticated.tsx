fe3f0f6705b901b450b3c5791184bb0a251cce25
André Ghisleni Raimann <andre@andreg.com.br>
2025-08-15 18:10:46 -0300

    Changes to be committed: 	deleted:    src/pages/(hidden-folder)/first-level.tsx 	deleted:    src/pages/(hidden-folder)/layouts/_hiddenLayout.tsx 	deleted:    src/pages/(hidden-folder)/layouts/_hiddenLayout/bar.tsx 	deleted:    src/pages/(hidden-folder)/layouts/_hiddenLayout/foo.tsx 	deleted:    src/pages/(hidden-folder)/layouts/visibleLayout.tsx 	deleted:    src/pages/(hidden-folder)/layouts/visibleLayout/bar.tsx 	deleted:    src/pages/(hidden-folder)/layouts/visibleLayout/foo.tsx 	modified:   src/pages/_authenticated.tsx 	deleted:    src/pages/foo/bar/baz.tsx 	modified:   src/pages/index.tsx 	deleted:    src/pages/pokemon/$id.tsx 	deleted:    src/pages/pokemon/index.tsx 	modified:   src/route-tree.gen.ts

--- src/pages/_authenticated.tsx
+++ src/pages/_authenticated.tsx
@@ -1,10 +1,11 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
+import { auth } from '@/lib/auth';
 
 export const Route = createFileRoute('/_authenticated')({
-  beforeLoad: ({ context }) => {
-    const { isLogged } = context.authentication;
-    if (!isLogged()) {
-      throw redirect({ to: '/login' });
+  beforeLoad: async () => {
+    const { data } = await auth.getSession();
+    if (!data) {
+      throw redirect({ to: '/sign-in' });
     }
   },
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { auth } from '@/lib/auth';
 
 export const Route = createFileRoute('/')({
   component: Home,
 });
 
 export function Home() {
+  const { data } = auth.getSession();
+
   return (
     <div>
       <h1>Home Page</h1>
