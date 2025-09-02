ed9c4fbbb9414bad6aa26afb5e0a1c0c789cade4
André Ghisleni Raimann <andre@andreg.com.br>
2025-09-02 17:35:43 -0300

    Changes to be committed: 	modified:   src/app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,6 @@
-import { AuthUIProvider } from '@daveyplate/better-auth-ui';
-import {
-  createRouter,
-  Link,
-  RouterProvider,
-  useRouter,
-} from '@tanstack/react-router';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { ThemeProvider } from './components/theme-provider';
 import { Toaster } from './components/ui/sonner';
-import { auth } from './lib/auth';
 import { routeTree } from './route-tree.gen';
 
 const router = createRouter({
@@ -24,19 +17,10 @@ declare module '@tanstack/react-router' {
 }
 
 export function App() {
-  const r = useRouter();
-
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AuthUIProvider
-        authClient={auth}
-        Link={({ href, ...props }) => <Link to={href} {...props} />}
-        navigate={(href) => r.navigate({ href })}
-        replace={(href) => r.navigate({ href, replace: true })}
-      >
-        <RouterProvider router={router} />
-        <Toaster />
-      </AuthUIProvider>
+      <RouterProvider router={router} />
+      <Toaster />
     </ThemeProvider>
   );
 }
